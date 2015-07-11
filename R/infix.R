bb12a5eb8719ccef9d4aecbceb877a3655ea4b10
robertzk <technoguyrob@gmail.com>
2015-07-10 22:02:14 -0500

    Add eachdo and mapdo

--- R/infix.R
+++ R/infix.R
@@ -6,6 +6,27 @@
 }
 
 #' @export
+#' @examples 1:5 %eachdo% { . + 1 }
+`%eachdo%` <- function(obj, expr) {
+  env <- parent.frame()
+  expr <- substitute(expr)
+  lapply(obj, function(x) {
+    eval(expr, envir = list2env(list(. = x), parent = env))
+  })
+  obj
+}
+
+#' @export
 #' @examples 1:5 %map% function(x) x + 1
 `%map%` <- lapply
+
+#' @export
+#' @examples 1:5 %mapdo% { . + 1 }
+`%mapdo%` <- function(obj, expr) {
+  env <- parent.frame()
+  expr <- substitute(expr)
+  lapply(obj, function(x) {
+    eval(expr, envir = list2env(list(. = x), parent = env))
+  })
+}
 
